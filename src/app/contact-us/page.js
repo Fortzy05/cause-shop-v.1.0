f6ccd3e76e7a40d98c73e54e65e4b7bf3bc6f300
Fortzy05 <omorodion.fortune@gmail.com>
2024-08-09 09:07:45 +0100

    fixes

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -321,7 +321,7 @@ export default function Page() {
           })}
         </div>
         <div className="flex justify-center">
-          <button className="w-[84px] h-[38px] text-[14px] leading-[18px] px-5 py-2 rounded-[50px] border-[1px] border-[#000000]">
+          <button className="w-[84px] h-[38px] text-[14px] leading-[18px] px-5 py-2.5 rounded-[50px] border-[1px] border-[#000000]">
             See all
           </button>
         </div>
